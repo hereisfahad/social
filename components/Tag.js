747a30e3f111c35d8337e285f275eee2b9f0b139
hereisfahad <fahadjabbar31@gmail.com>
2021-04-11 17:38:59 +0500

    design profile page

---
+++ components/Tag.js
@@ -0,0 +1,7 @@
+const Tag = ({ children }) => (
+    <div className="px-3 py-1 mb-2 mr-2 text-xs font-semibold tracking-widest text-white bg-green-500 rounded-full">
+        {children}
+    </div>
+)
+
+export default Tag
