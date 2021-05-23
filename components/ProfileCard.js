9412f53fd33c2a243dbc54ae7b284003e082dfbb
Fahad Jabbar <fahadjabbar31@gmail.com>
2021-05-24 01:30:33 +0500

    integrate api for profiles page

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,36 +1,37 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-import Tag from '@/components/Tag'
+import Tag from "@/components/Tag";
 
-function ProfileCard() {
-    return (
-        <div className="flex flex-col items-center justify-center max-w-lg px-6 py-6 space-y-2 bg-gray-100 border border-gray-200 rounded w-94 sm:px-8 sm:space-x-6 sm:justify-start sm:flex-row">
-            <div className="flex items-center justify-center p-1 bg-white rounded-full">
-                <Image
-                    className="rounded-full"
-                    src="http://www.gravatar.com/avatar/e7e0756ace3878e35aaa6cfc9dbcef50?s=200&d=mm&r=pg"
-                    alt="Picture of the author"
-                    width={100}
-                    height={100}
-                />
-            </div>
-            <div className="flex flex-col items-center justify-start space-y-2 sm:items-baseline">
-                <h2 className="text-xl font-bold">Dummy Name</h2>
-                <p className="text-gray-800">Lorem ipsum lorem lorem</p>
-                <div className="flex flex-wrap items-center justify-center">
-                    <Tag>HTML</Tag>
-                    <Tag>CSS</Tag>
-                    <Tag>JS</Tag>
-                </div>
-                <Link href="#">
-                    <a className="px-3 py-2 text-sm font-semibold tracking-wider text-white uppercase bg-purple-500 rounded hover:bg-purple-400 focus:outline-none focus-within:ring-2 ring-purple-600">
-                        View Profile
-                </a>
-                </Link>
-            </div>
+function ProfileCard(props) {
+  const { user, skills } = props;
+  return (
+    <div className="flex flex-col items-center justify-center max-w-lg px-6 py-6 space-y-2 bg-gray-100 border border-gray-200 rounded w-94 sm:px-8 sm:space-x-6 sm:justify-start sm:flex-row">
+      <div className="flex items-center justify-center p-1 bg-white rounded-full">
+        <Image
+          className="rounded-full"
+          src={`https:${user.avatar}`}
+          alt={`Picture of ${user.name}`}
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="flex flex-col items-center justify-start space-y-2 sm:items-baseline">
+        <h2 className="text-xl font-bold">{user.name}</h2>
+        <p className="text-gray-800">I would love to have some chemistry(bio)</p>
+        <div className="flex flex-wrap items-center justify-center">
+          {skills.map((skill) => (
+            <Tag key={skill}>{skill}</Tag>
+          ))}
         </div>
-    )
+        <Link href={`/profile/${user._id}`}>
+          <a className="px-3 py-2 text-sm font-semibold tracking-wider text-white uppercase bg-purple-500 rounded hover:bg-purple-400 focus:outline-none focus-within:ring-2 ring-purple-600">
+            View Profile
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
--- pages/profiles.js
+++ pages/profiles.js
@@ -1,17 +1,42 @@
+import axios from "@/lib/axios";
 import ProfileCard from "@/components/ProfileCard";
 
-function profiles() {
-    return (
-        <div className="flex flex-col items-center flex-1 px-4 py-10 sm:p-8">
-            <h2 className="text-3xl font-bold text-gray-900">Developers</h2>
-            <p className="font-semibold text-gray-700">Browse and connect with Developers</p>
-            <div className="grid grid-cols-1 gap-6 mt-8">
-                <ProfileCard />
-                <ProfileCard />
-                <ProfileCard />
-            </div>
-        </div>
-    )
+function profiles({ profiles }) {
+  return (
+    <div className="flex flex-col items-center flex-1 px-4 py-10 sm:p-8">
+      <h2 className="text-3xl font-bold text-gray-900">Developers</h2>
+      <p className="font-semibold text-gray-700">
+        Browse and connect with Developers
+      </p>
+      <div className="grid grid-cols-1 gap-6 mt-8">
+          {
+            profiles.map(profile => (
+                <ProfileCard key={profile._id} {...profile} />
+            ))
+          }
+      </div>
+    </div>
+  );
 }
 
-export default profiles
+export default profiles;
+
+export async function getServerSideProps({ req, res }) {
+  try {
+    const { data } = await axios({
+      method: "get",
+      url: "/profile/all",
+      headers: {
+        cookie: req.headers.cookie,
+      },
+    });
+    return {
+      props: { profiles: data },
+    };
+  } catch (error) {
+    res.setHeader("location", "/login");
+    res.statusCode = 302;
+    res.end();
+  }
+}
+
