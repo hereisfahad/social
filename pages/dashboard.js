e53959f871821e898f56194ed21eca9d17c17e4b
Fahad Jabbar <fahadjabbar31@gmail.com>
2021-05-27 23:50:36 +0500

    feat: update profile (#9)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,38 +1,45 @@
 import axios from "@/lib/axios";
+import NextLink from "next/link";
+
 import EducationTable from "@/components/EducationTable";
 import ExperienceTable from "@/components/ExperienceTable";
 
 export default function Dashboard({ profile }) {
-  const { experience, education } = profile
+  const { experience, education } = profile;
   return (
     <div className="flex flex-col items-center justify-center flex-1 p-4 space-y-8 bg-gray-50 sm:px-6 lg:px-8">
       <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
         Dashboard
       </h2>
+      <NextLink href="/update-profile">
+        <a className="px-5 py-2 font-medium text-white transition duration-300 bg-purple-700 border border-purple-500 rounded select-none hover:bg-white focus:outline-none focus-within:ring ring-purple-600 hover:text-purple-500">
+          Edit Profile
+        </a>
+      </NextLink>
       <ExperienceTable experience={experience} />
       <EducationTable education={education} />
     </div>
-  )
+  );
 }
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps({ req }) {
   try {
     const { data } = await axios({
-      method: 'get',
-      url: '/profile/me',
+      method: "get",
+      url: "/profile/me",
       headers: {
-        cookie: req.headers.cookie
-      }
-    })
+        cookie: req.headers.cookie,
+      },
+    });
     return {
-      props: { profile: data }
-    }
+      props: { profile: data },
+    };
   } catch (error) {
     return {
       redirect: {
         permanent: false,
-        destination: '/login'
-      }
-    }
+        destination: "/login",
+      },
+    };
   }
 }
